eda8c414bd081ffca56034c3cd5bf3a83a3a7f55
Hemanth datta <99961202+Henryhose@users.noreply.github.com>
2023-08-16 13:19:04 +0530

    Update settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'your-secret-key-goes-here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
