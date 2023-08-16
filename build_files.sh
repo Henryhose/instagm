29aec5da45c0c13054402d15397500129cf15f5e
henryhose <kothurihemanthdatta@gmail.com>
2023-08-16 12:13:11 +0530

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,5 @@
+# Install Python packages from requirements.txt
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Run Django's collectstatic command
+python manage.py collectstatic
