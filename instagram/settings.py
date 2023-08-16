40fb7d56899b38f4d0bdf502c4725a839123f6ad
Hemanth datta <99961202+Henryhose@users.noreply.github.com>
2023-08-16 12:56:39 +0530

    Update settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'productionfiles', 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'instagm/static')]  # Changed to a list
 STATIC_URL = '/static/'
 
