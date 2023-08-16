b9c77a9a7e02f867878cb86ae7dc648af3dc3808
Hemanth datta <99961202+Henryhose@users.noreply.github.com>
2023-08-16 12:25:34 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,19 @@
+cd instagram
+
 pip install -r requirements.txt
-python manage.py collectstatic
+
+npm install  # Install Node.js dependencies
+npm run build  # Run build process
+
+# Run database migrations
+python manage.py migrate
+
+# Set environment variables
+export DJANGO_SECRET_KEY='your-secret-key'
+export DJANGO_DEBUG='True'
+
+# Or, load environment variables from a .env file
+# source .env
+
+# Run tests (if applicable)
+python manage.py test
