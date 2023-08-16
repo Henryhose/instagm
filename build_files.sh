1e9e44a2e6446be0985d3916a5443d58a9ea03e2
Hemanth datta <99961202+Henryhose@users.noreply.github.com>
2023-08-16 12:50:52 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,27 @@
+#!/bin/bash
+
+# This script performs build-related tasks for your project
+
+# Step 1: Install Python dependencies
 pip install -r requirements.txt
-python3.11 manage.py collectstatic
+
+# Step 2: Run database migrations
+python manage.py migrate
+
+# Step 3: Collect static files
+python manage.py collectstatic --noinput
+
+# Step 4: Additional build tasks (customize as needed)
+# For example, you might want to build frontend assets here.
+
+# Step 5: Run tests (optional)
+# python manage.py test
+
+# Step 6: Set environment variables (if needed)
+# export DJANGO_SECRET_KEY='your-secret-key'
+# export DJANGO_DEBUG='False'
+
+# Step 7: Run other build-related tasks as needed
+
+# Step 8: Complete the build process
+echo "Build completed successfully"
